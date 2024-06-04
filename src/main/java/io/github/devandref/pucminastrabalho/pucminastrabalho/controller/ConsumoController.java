6d1d8c0809ab1a0aab74083b8f56540ae82df7d2
devandref <yoandredepaula@gmail.com>
2024-06-03 23:47:36 -0300

    implement

--- src/main/java/io/github/devandref/pucminastrabalho/pucminastrabalho/controller/ConsumoController.java
+++ src/main/java/io/github/devandref/pucminastrabalho/pucminastrabalho/controller/ConsumoController.java
@@ -1,14 +1,14 @@
 package io.github.devandref.pucminastrabalho.pucminastrabalho.controller;
 
 import io.github.devandref.pucminastrabalho.pucminastrabalho.model.ConsumoCombustivel;
+import io.github.devandref.pucminastrabalho.pucminastrabalho.model.dto.ConsumoCombustivelDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -21,8 +21,48 @@ public class ConsumoController {
         return this.consumoList;
     }
 
-    @PostMapping("/consumos")
-    public ResponseEntity saveCombustivel(@RequestMapping ) {
+    @GetMapping("/consumos/{id}")
+    public ResponseEntity<ConsumoCombustivel> findById(@PathVariable String id) {
+        Optional<ConsumoCombustivel> consumoCombustivelOptional = this.consumoList.stream().filter(consumoCombustivel -> consumoCombustivel.getId().toString().equals(id)).findFirst();
+        if (consumoCombustivelOptional.isPresent()) {
+            return ResponseEntity.ok(consumoCombustivelOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
 
+    @PutMapping("/consumos/{id}")
+    public ResponseEntity updateById(@PathVariable String id, @RequestBody ConsumoCombustivelDTO consumoCombustivelDTO) {
+        Optional<ConsumoCombustivel> consumoCombustivelOptional = this.consumoList.stream().filter(consumoCombustivel -> consumoCombustivel.getId().toString().equals(id)).findFirst();
+        if (consumoCombustivelOptional.isPresent()) {
+            this.consumoList.remove(consumoCombustivelOptional.get());
+            ConsumoCombustivel consumoCombustivel = consumoCombustivelOptional.get();
+            if (consumoCombustivelDTO.getTipoCombustivel() != null) {
+                consumoCombustivel.setTipo(consumoCombustivelDTO.getTipoCombustivel());
+            }
+            if (consumoCombustivelDTO.getValor() != null) {
+                consumoCombustivel.setValor(consumoCombustivelDTO.getValor());
+            }
+            this.consumoList.add(consumoCombustivel);
+            return ResponseEntity.ok(consumoCombustivel);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/consumos")
+    public ResponseEntity saveCombustivel(@RequestBody ConsumoCombustivelDTO consumoCombustivelDTO, UriComponentsBuilder uriComponent) {
+        ConsumoCombustivel consumoCombustivel = ConsumoCombustivel.dtoToEntity(consumoCombustivelDTO);
+        this.consumoList.add(consumoCombustivel);
+        var uri = uriComponent.path("/consumos/{id}").buildAndExpand(consumoCombustivel.getId()).toUri();
+        return ResponseEntity.created(uri).body(consumoCombustivel);
+    }
+
+    @DeleteMapping("/consumos/{id}")
+    public ResponseEntity deleteById(@PathVariable String id) {
+        Optional<ConsumoCombustivel> consumoCombustivelOptional = this.consumoList.stream().filter(consumoCombustivel -> consumoCombustivel.getId().toString().equals(id)).findFirst();
+        if (consumoCombustivelOptional.isPresent()) {
+            this.consumoList.remove(consumoCombustivelOptional.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
---
+++ src/main/java/io/github/devandref/pucminastrabalho/pucminastrabalho/model/dto/ConsumoCombustivelDTO.java
@@ -0,0 +1,23 @@
+package io.github.devandref.pucminastrabalho.pucminastrabalho.model.dto;
+
+public class ConsumoCombustivelDTO {
+
+    private Double valor;
+    private String tipoCombustivel;
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+}
